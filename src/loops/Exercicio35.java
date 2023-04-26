4bee36d4f7a061d3aff839eae6efd1c22810daed
jssicakisner <jessicakisner@hotmail.com>
2023-04-26 20:57:39 -0300

    update

---
+++ src/loops/Exercicio35.java
@@ -0,0 +1,28 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Exercicio35 {
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Informe um numero inteiro: ");
+                int N = scan.nextInt();
+
+                System.out.println("Os numeros primos no intervalo entre 1 e " + N + " sao:");
+
+                int primo = 0;
+
+                for (int i = 1; i <= N; i++) {
+                    for (int j = 1; j <= i; j++) {
+                        if (i % j == 0) {
+                            primo++;
+                        }
+                    }
+                 if (primo == 2) {            
+                    System.out.println(i);   
+                }
+                primo = 0;            
+                }
+        }
+    }
+}
