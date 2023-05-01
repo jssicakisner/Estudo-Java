30e80df6a4e901c2cda2c1e62dfb673d99813da1
jssicakisner <jessicakisner@hotmail.com>
2023-05-01 20:38:23 -0300

    update

---
+++ src/loops/Exercicio45.java
@@ -0,0 +1,10 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Exercicio45 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+          
+    }
+}
