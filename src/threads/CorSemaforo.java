028e276abb68e7c8d986324e41be103d4d8d7460
jssicakisner <jessicakisner@hotmail.com>
2023-06-20 15:18:52 -0300

    threads

---
+++ src/threads/CorSemaforo.java
@@ -0,0 +1,5 @@
+package threads;
+
+public enum CorSemaforo {
+    VERDE, AMARELO, VERMELHO;
+}
---
+++ src/threads/ThreadSemaforo.java
@@ -0,0 +1,78 @@
+package threads;
+
+public class ThreadSemaforo implements Runnable {
+    private CorSemaforo cor;
+    private boolean parar;
+    private boolean corMudou;
+
+    public ThreadSemaforo() {
+
+        this.cor = CorSemaforo.VERMELHO;
+        this.parar = false;
+        this.corMudou = false;
+
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        while (!parar) {
+            try {
+                switch (this.cor) {
+                    case VERMELHO:
+                        Thread.sleep(2000);
+                        break;
+                    case AMARELO:
+                        Thread.sleep(500);
+                        break;
+                    case VERDE:
+                        Thread.sleep(3000);
+                        break;
+                    default:
+                        break;
+                }
+                this.mudarCor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private synchronized void mudarCor() {
+        switch (this.cor) {
+            case VERMELHO:
+                this.cor = CorSemaforo.VERDE;
+                break;
+            case VERDE:
+                this.cor = CorSemaforo.AMARELO;
+                break;
+            case AMARELO:
+                this.cor = CorSemaforo.VERMELHO;
+                break;
+            default:
+                break;
+        }
+        this.corMudou = true;
+        notify();
+    }
+
+    public synchronized void esperaCorMudar() {
+        while (!this.corMudou) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        this.corMudou = false;
+    }
+
+    public synchronized void desligarSemaforo() {
+        this.parar = true;
+    }
+
+    public CorSemaforo getCor() {
+        return cor;
+    }
+
+}
